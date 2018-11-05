ed17f36cfaba9abd44c5013e1a8f86eb84f77ac4
S Ravisangar <sravisangar@ss-macbook-pro.local>
2018-11-05 14:57:30 +0000

    Working Version (Drag and Drop)

--- Ravi.Ex41/DragImageView.swift
+++ Ravi.Ex41/DragImageView.swift
@@ -10,30 +10,21 @@ import UIKit
 
 class DragImageView: UIImageView {
 
+    var startLocation: CGPoint?
+    var myDelegate: subviewDelegate?
     
-    
-    
-   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-      let startLocation = (touches.first?.locationInView(self)
-        
-    )
-
-       
-      
-        
+        startLocation = touches.first?.location(in: self)
+    }
  
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let currentLocation = touches.first?.locationInView(self)
-//        let dx = currentLocation!.x - startLocation!.x
-//        let dy  = currentLocation!.y - startLocation!.y
-//
-//
-//
-//        self.center = CGPoint(x:self.center.x+dx,y:self.center.y+dy)
-//
-//
-//        
-//    }
+   
+            self.myDelegate?.changeSomething()
+            let currentLocation = touches.first?.location(in: self)
+            let dx = currentLocation!.x - startLocation!.x
+            let dy  = currentLocation!.y - startLocation!.y
+            self.center = CGPoint(x:self.center.x+dx,y:self.center.y+dy)
+    }
     
 }
--- Ravi.Ex41/ViewController.swift
+++ Ravi.Ex41/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol subviewDelegate {
+    func changeSomething()
+}
 
+class ViewController: UIViewController, subviewDelegate {
+
+    @IBOutlet weak var flower: DragImageView!
+    @IBOutlet weak var information: UITextField!
+    
+    func changeSomething () {
+        information.text = "X Position"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        flower.myDelegate = self
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
